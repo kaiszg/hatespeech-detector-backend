6e875d8e49a118e26938c812d44962165f9181f5
kaiszg <kais.zgal@gmail.com>
2018-04-19 19:37:59 +0200

    create spring boot project

---
+++ src/main/java/de/beuthhochschule/hatespeech/api/HatespeechDetectorApiApplication.java
@@ -0,0 +1,12 @@
+package de.beuthhochschule.hatespeech.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatespeechDetectorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatespeechDetectorApiApplication.class, args);
+	}
+}
