f3649231eb4139fe4b148bc013da4d071aa4a8d2
Kais <kais.zgal@gmail.com>
2018-05-06 19:04:21 +0200

    use non-primitive type for attribute score to allow null

--- src/main/java/de/beuthhochschule/hatespeech/api/model/Comment.java
+++ src/main/java/de/beuthhochschule/hatespeech/api/model/Comment.java
@@ -6,6 +6,7 @@
 package de.beuthhochschule.hatespeech.api.model;
 
 import java.util.Date;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -31,7 +32,7 @@ public class Comment {
 	private String text;
 	
 	@Column(name="score")
-	private double score;
+	private Float score;
 	
 	@Column(name="timestamp")
 	private Date timestamp;
@@ -58,11 +59,11 @@ public class Comment {
 		this.text = text;
 	}
 
-	public double getScore() {
+	public Float getScore() {
 		return score;
 	}
 
-	public void setScore(double score) {
+	public void setScore(Float score) {
 		this.score = score;
 	}
 
@@ -91,56 +92,21 @@ public class Comment {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((label == null) ? 0 : label.hashCode());
-		long temp;
-		temp = Double.doubleToLongBits(score);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + ((text == null) ? 0 : text.hashCode());
-		result = prime * result + ((timestamp == null) ? 0 : timestamp.hashCode());
-		result = prime * result + ((url == null) ? 0 : url.hashCode());
-		return result;
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Comment comment = (Comment) o;
+		return Objects.equals(id, comment.id) &&
+				Objects.equals(text, comment.text) &&
+				Objects.equals(score, comment.score) &&
+				Objects.equals(timestamp, comment.timestamp) &&
+				Objects.equals(label, comment.label) &&
+				Objects.equals(url, comment.url);
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Comment other = (Comment) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (label == null) {
-			if (other.label != null)
-				return false;
-		} else if (!label.equals(other.label))
-			return false;
-		if (Double.doubleToLongBits(score) != Double.doubleToLongBits(other.score))
-			return false;
-		if (text == null) {
-			if (other.text != null)
-				return false;
-		} else if (!text.equals(other.text))
-			return false;
-		if (timestamp == null) {
-			if (other.timestamp != null)
-				return false;
-		} else if (!timestamp.equals(other.timestamp))
-			return false;
-		if (url == null) {
-			if (other.url != null)
-				return false;
-		} else if (!url.equals(other.url))
-			return false;
-		return true;
+	public int hashCode() {
+
+		return Objects.hash(id, text, score, timestamp, label, url);
 	}
 }
