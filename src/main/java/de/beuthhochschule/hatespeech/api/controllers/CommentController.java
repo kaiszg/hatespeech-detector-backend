265b28fdf3edfc946b8e42369e2c9c3ce5d3aed8
Kais <kais.zgal@gmail.com>
2018-05-06 19:03:29 +0200

    return meaningful http status

--- src/main/java/de/beuthhochschule/hatespeech/api/controllers/CommentController.java
+++ src/main/java/de/beuthhochschule/hatespeech/api/controllers/CommentController.java
@@ -14,6 +14,8 @@ import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurat
 import org.deeplearning4j.nn.modelimport.keras.exceptions.UnsupportedKerasConfigurationException;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,26 +40,38 @@ public class CommentController {
 	private CommentService commentService;
 	
 	@GetMapping
-	public List<Comment> getAllComments() {
-		return commentService.findAllByOrderScore();
+	public ResponseEntity<List<Comment>> getAllComments() {
+        List<Comment> comments = commentService.findAllByOrderScore();
+        if(comments == null || comments.isEmpty()) {
+            return new ResponseEntity<List<Comment>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<Comment>>(comments, HttpStatus.OK);
 	}
 
 	@GetMapping("/labelled")
-	public List<Comment> getLabelledComments() {
-		return commentService.findLabelledCommentsOrderByScore();
+	public ResponseEntity<List<Comment>> getLabelledComments() {
+	    List<Comment> comments = commentService.findLabelledCommentsOrderByScore();
+	    if(comments == null || comments.isEmpty()) {
+            return new ResponseEntity<List<Comment>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<Comment>>(comments, HttpStatus.OK);
 	}
 	
 	@GetMapping("/unlabelled")
-	public List<Comment> getUnlabelledComments() {
-		return commentService.findUnlabelledCommentsOrderByScore();
+	public ResponseEntity<List<Comment>> getUnlabelledComments() {
+        List<Comment> comments = commentService.findUnlabelledCommentsOrderByScore();
+        if(comments == null || comments.isEmpty()) {
+            return new ResponseEntity<List<Comment>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<Comment>>(comments, HttpStatus.OK);
 	}
 	
 	@PostMapping
-	public Comment createComment(@RequestBody Comment comment) /* throws IOException, InvalidKerasConfigurationException, UnsupportedKerasConfigurationException */ {
+	public ResponseEntity<Comment> createComment(@RequestBody Comment comment) /* throws IOException, InvalidKerasConfigurationException, UnsupportedKerasConfigurationException */ {
 		// TODO import Keras-Model and predict score
 		// MultiLayerNetwork network = KerasModelImport.importKerasSequentialModelAndWeights("D:\\Studium\\EDM\\test_model.h5");
 		// System.out.println(network.conf().toJson());
-		return commentService.createComment(comment);
+        return new ResponseEntity<Comment>(commentService.createComment(comment), HttpStatus.CREATED);
 	}
 	
 	@GetMapping("/init")
@@ -66,7 +80,7 @@ public class CommentController {
 		first.setText("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
 		first.setTimestamp(new Date());
 		first.setUrl("http://www.heise.de/comments/145268");
-		first.setScore(0.56);
+		first.setScore(new Float(0.56));
 
 		Comment second = new Comment();
 		second.setText("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   \r\n" + 
@@ -76,20 +90,20 @@ public class CommentController {
 				"Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse");
 		second.setTimestamp(new Date());
 		second.setUrl("http://www.heise.de/comments/568456367");
-		second.setScore(0.7456);
+		second.setScore(new Float(0.7456));
 
 		Comment third = new Comment();
 		third.setText("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
 		third.setTimestamp(new Date());
 		third.setUrl("http://www.heise.de/comments/56756845");
-		third.setScore(0.234);
+		third.setScore(new Float(0.234));
 		third.setLabel("not deleted");
 
 		Comment fourth = new Comment();
 		fourth.setText("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
 		fourth.setTimestamp(new Date());
 		fourth.setUrl("http://www.heise.de/comments/8956845763");
-		fourth.setScore(0.987);
+		fourth.setScore(new Float(0.987));
 		fourth.setLabel("deleted");
 
 		commentService.createComment(first);
